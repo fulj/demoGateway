f838242299105b0a5c7f76800fd83e08ff37435e
ljfu <wb-flj593682@alibaba-inc.com>
2022-04-28 17:58:26 +0800

    first commit

---
+++ src/main/java/com/example/demoGateway/DemoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGatewayApplication.class, args);
+	}
+
+}
