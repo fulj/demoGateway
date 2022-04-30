7e74f8ab3f4876daf089c20b8112b582ea8641c0
ljfu <wb-flj593682@alibaba-inc.com>
2022-04-30 11:47:50 +0800

    test

--- src/main/java/com/example/demoGateway/DemoGatewayApplication.java
+++ src/main/java/com/example/demoGateway/DemoGatewayApplication.java
@@ -2,6 +2,8 @@ package com.example.demoGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class DemoGatewayApplication {
@@ -9,5 +11,9 @@ public class DemoGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGatewayApplication.class, args);
 	}
+
+//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//		return builder.routes().build()
+//	}
 
 }
---
+++ src/main/java/com/example/demoGateway/FluxTest.java
@@ -0,0 +1,38 @@
+package com.example.demoGateway;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FluxTest {
+
+    public static void main(String[] args) {
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        System.out.println("onSubscribe");
+                        s.request(Long.MAX_VALUE);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        elements.add(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {}
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("complete");
+                    }
+                });
+        System.out.println(elements.size());
+    }
+}
